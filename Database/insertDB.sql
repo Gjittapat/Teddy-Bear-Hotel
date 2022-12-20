0c75897179c595f6115fc3ead45d92e159ef6309
Gjittapat <112146676+Gjittapat@users.noreply.github.com>
2022-12-20 15:09:10 +0700

    Add files via upload

---
+++ Database/insertDB.sql
@@ -0,0 +1,25 @@
+INSERT INTO room(room_no, availability_status) VALUES (1,"Available"),(2,"Available"),(3,"Available"),(4,"Available"),(5,"Available"),(6,"Available"),(7,"Available"),(8,"Available"),(9,"Available"),(10,"Available");
+
+insert into Room_Category (Room_No, Room_type, Bed_type, Rate) values
+(1, 'deluxe', 'single', 3500), (2, 'junior suite', 'double', 4500), (3, 'deluxe', 'double', 3500),
+(4, 'junior suite', 'single', 4500), (5, 'superior suite', 'double', 5500), (6, 'superior', 'single', 5500),
+(7, 'deluxe', 'single', 3500), (8, 'superior suite', 'double', 5500), (9, 'junior suite', 'double', 4500),
+(10, 'superior', 'double', 5500);
+select * from room_category;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
